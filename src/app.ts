89a130c65613bae1eb275d8b4aa1d3958d99a2f1
Nirakarr <niraakar42@gmail.com>
2026-08-23 17:42:11 +0545

    Add: origin for cors from .env

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import itemRoutes from "./routes/saved-item.routes";
@@ -6,7 +7,11 @@ import tagRoutes from "./routes/tag.routes";
 const app: Application = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN ?? "http://localhost:3000",
+  }),
+);
 app.use(express.json());
 
 // Routes
